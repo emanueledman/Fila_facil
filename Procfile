cd108d4fff95b6a882aa935f908ce3adbd658980
Emanuel Joao <emanuelhedima@gmail.com>
2025-04-19 19:07:16 +0100

    primeiro yteste

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 # Procfile
-web: gunicorn --worker-class eventlet -w 1 wsgi:app
+web: gunicorn --worker-class eventlet -w 1 --bind 0.0.0.0:$PORT wsgi:app
--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,11 @@
 # wsgi.py
 import os
-from app import create_app
+from ini import create_app
 
 # Criar a aplicação Flask
 app, _ = create_app()
 
 if __name__ == "__main__":
     # Apenas para testes locais, o Gunicorn será usado em produção
-    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
